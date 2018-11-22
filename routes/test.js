59ea6bdf6c3777c7a76587a5d155c9e83f02d9e0
tsmack <tony.m@afrihost.com>
2018-11-22 20:53:25 +0200

    test route

--- routes/test.js
+++ routes/test.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
     console.log(req.ip);
-    res.post(req.ip)
+    res.json(req.ip)
 });
 
 module.exports = router;
